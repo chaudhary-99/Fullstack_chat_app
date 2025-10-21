43d0a9a05a4eadeda9f47b81f58d272e1031d1b2
Karishma Chaudhary <Chaudharykarishma050122@gmail.com>
2025-10-21 21:35:09 +0530

    add secure backend live link

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "http://fullstack-chat-qmx5lm0dp-karishma-chaudharys-projects.vercel.app",
+      : "https://fullstack-chat-qmx5lm0dp-karishma-chaudharys-projects.vercel.app",
   withCredentials: true,
 });
